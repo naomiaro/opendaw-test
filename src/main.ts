dd26c4333726a61aff2f15d5f40bbd6b29e5bc69
André Michelle <andre.michelle@gmail.com>
2025-07-16 15:33:16 +0200

    update to sdk 0.0.11 and create project manually with ProjectApi

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
+// noinspection PointlessArithmeticExpressionJS
+
 import {assert, ProgressHandler, UUID} from "@opendaw/lib-std"
+import {PPQN} from "@opendaw/lib-dsp"
 import {Promises} from "@opendaw/lib-runtime"
+import {NoteEventBox, NoteEventCollectionBox, NoteRegionBox} from "@opendaw/studio-boxes"
 import {AudioData, SampleMetaData} from "@opendaw/studio-adapters"
-import {MainThreadSampleManager, Project, SampleProvider, WorkerAgents, Worklets} from "@opendaw/studio-core"
+import {
+    InstrumentFactories,
+    MainThreadSampleManager,
+    Project,
+    SampleProvider,
+    WorkerAgents,
+    Worklets
+} from "@opendaw/studio-core"
 import {testFeatures} from "./features"
 import {SampleApi} from "./SampleApi"
 
@@ -32,17 +43,48 @@ import WorkletsUrl from "@opendaw/studio-core/processors.js?url"
         }
     }
     {
-        const provider: SampleProvider = {
-            fetch: (uuid: UUID.Format, progress: ProgressHandler): Promise<[AudioData, SampleMetaData]> =>
-                SampleApi.load(context, uuid, progress)
+        const sampleProvider = new class implements SampleProvider {
+            fetch(uuid: UUID.Format, progress: ProgressHandler): Promise<[AudioData, SampleMetaData]> {
+                return SampleApi.load(context, uuid, progress)
+            }
         }
-        const sampleManager = new MainThreadSampleManager(provider, context)
-        const project = Project.load({sampleManager}, await fetch("subset.od").then(x => x.arrayBuffer()))
+        const sampleManager = new MainThreadSampleManager(sampleProvider, context)
+        const project = Project.new({sampleManager})
         const worklet = Worklets.get(context).createEngine(project)
         await worklet.isReady()
         while (!await worklet.queryLoadingComplete()) {}
         worklet.connect(context.destination)
         worklet.isPlaying().setValue(true)
+        const {boxGraph, editing, api} = project
+        editing.modify(() => {
+            const {trackBox: {regions}} = api.createInstrument(InstrumentFactories.Vaporisateur)
+            const collectionBox = NoteEventCollectionBox.create(boxGraph, UUID.generate())
+            NoteEventBox.create(boxGraph, UUID.generate(), box => {
+                box.position.setValue(PPQN.SemiQuaver * 0)
+                box.duration.setValue(PPQN.SemiQuaver * 1)
+                box.pitch.setValue(60)
+                box.events.refer(collectionBox.events)
+            })
+            NoteEventBox.create(boxGraph, UUID.generate(), box => {
+                box.position.setValue(PPQN.SemiQuaver * 1)
+                box.duration.setValue(PPQN.SemiQuaver * 1)
+                box.pitch.setValue(63)
+                box.events.refer(collectionBox.events)
+            })
+            NoteEventBox.create(boxGraph, UUID.generate(), box => {
+                box.position.setValue(PPQN.SemiQuaver * 2)
+                box.duration.setValue(PPQN.SemiQuaver * 1)
+                box.pitch.setValue(65)
+                box.events.refer(collectionBox.events)
+            })
+            NoteRegionBox.create(boxGraph, UUID.generate(), box => {
+                box.position.setValue(PPQN.Quarter * 0)
+                box.duration.setValue(PPQN.Bar * 4)
+                box.loopDuration.setValue(PPQN.Quarter * 1)
+                box.events.refer(collectionBox.owners)
+                box.regions.refer(regions)
+            })
+        })
     }
     if (context.state === "suspended") {
         window.addEventListener("click",
